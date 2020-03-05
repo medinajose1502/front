2882b676aa572469a5223a332e32944f08dcbb99
José Medina <medinajose1502@gmail.com>
2020-03-05 18:24:35 -0400

    tabla que tiene sentido

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -14,10 +14,10 @@
     data() {
       return {
         items: [
-          { codigo: 40, nombre: 'Dickerson', categoria: 'Macdonald', precio: 1000},
-          { codigo: 21, nombre: 'Larsen', categoria: 'Shaw', precio: 1000},
-          { codigo: 89, nombre: 'Geneva', categoria: 'Wilson', precio: 1000},
-          { codigo: 38, nombre: 'Jami', categoria: 'Carney', precio: 1000}
+          { codigo: 40, nombre: 'Dell G5SE', categoria: 'Computadoras', precio: 1000},
+          { codigo: 21, nombre: 'Samsung Galaxy S20', categoria: 'Celulares', precio: 1000},
+          { codigo: 89, nombre: 'Asus RoG Swift', categoria: 'Monitores', precio: 1000},
+          { codigo: 38, nombre: 'Windows 10', categoria: 'Software', precio: 1000}
         ]
       }
     }
